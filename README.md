3e549c5c92ae8dbf2f2c56dcc1c9e21e0fa249a0
Jaeho <49357353+jeaho0613@users.noreply.github.com>
2020-03-25 00:37:00 +0900

    Intro 씬 완성
    - Dotween 활용
    - 코루틴

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # Unity_Self_Project
 
-cook ramen project
+프로젝트명: J Project #1  
+게임 이름 : Recycling Agent
 
-1024 x 768 크기
+1920 x 1080 크기  
+19:6 비율
 
 - 사용한 에셋
-  - https://cactus.tistory.com/193 (길형진 폰트)
-  - Pixel Font - Tripfive 에셋
-  - DOTween 에셋
-  - FREE Casual Game SFX Pack 에셋 (sound)
-  - Free Pixel Font - Thaleah
-  - Casual Game BGM #5
-  - Achievement SFX FREE
-  - RPG BGM - Lobby
+  - **폰트**
+    - https://cactus.tistory.com/193 (길형진 폰트)
+    - Pixel Font - Tripfive 에셋
+    - Free Pixel Font - Thaleah  
+  - **이미지 소스**
+    - RPG BGM - Lobby
+    - Sunny Land
+  - **사운트 소스**
+    - FREE Casual Game SFX Pack 에셋 (sound)
+    - Casual Game BGM #5
+  - **이팩트 소스**
+    - Achievement SFX FREE
+  - **엔진 소스**
+    - DOTween 에셋
+
+0325 Intro 씬 완성
---
+++ pay_table/Assets/pay.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pay : MonoBehaviour
+{
+    public InputField inputField;
+    public Text totalPay, text2;
+    public float gold;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void okBnt()
+    {
+        string strPay = inputField.text;
+        float pay = float.Parse(strPay);
+        gold += checkPay(pay);
+        totalPay.text = "총 급여 : " + gold.ToString();
+
+    }
+
+    public float checkPay(float pay)
+    {
+        if (pay >= 110)
+        {
+            pay = pay * 300;
+            Debug.Log($"pay * 300 으로 계산됐습니다. : {pay}");
+            text2.text = $"pay * 300 으로 계산됐습니다. : {pay}";
+            return pay;
+        }
+        else if (pay >= 90 && pay <= 109)
+        {
+            pay = pay * 300 * 0.9f;
+            Debug.Log($"pay * 300 * 0.9f 으로 계산됐습니다. : {pay}");
+            text2.text = $"pay * 300 * 0.9f 으로 계산됐습니다. : {pay}";
+            return pay;
+        }
+        else if (pay >= 70 && pay <= 89)
+        {
+            pay = pay * 300 * 0.8f;
+            Debug.Log($"pay * 300 * 0.8f 으로 계산됐습니다. : {pay}");
+            text2.text = $"pay * 300 * 0.8f 으로 계산됐습니다. : {pay}";
+            return pay;
+        }
+        else if (pay >= 50 && pay <= 69)
+        {
+            pay = pay * 300 * 0.7f;
+            Debug.Log($"pay * 300 * 0.7f으로 계산됐습니다. : {pay}");
+            text2.text = $"pay * 300 * 0.7f으로 계산됐습니다. : {pay}";
+            return pay;
+        }
+        else if (pay >= 30 && pay <= 49)
+        {
+            pay = pay * 300 * 0.6f;
+            Debug.Log($"pay * 300 * 0.6f으로 계산됐습니다. : {pay}");
+            text2.text = $"pay * 300 * 0.6f으로 계산됐습니다. : {pay}";
+            return pay;
+        }
+        else
+        {
+            Debug.LogError("범주에서 벗어납니다.");
+            text2.text = $"범주에서 벗어납니다. : {pay}";
+        }
+        return pay;
+    }
+}
