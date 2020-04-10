ece23ea3cde3f8cd2887d697e1db25f4d7869b83
Jaeho <49357353+jeaho0613@users.noreply.github.com>
2020-04-10 17:40:52 +0900

    Readme 참고
    - Axe 게임 진행 중지 // 차후 다시 개발
    - Colour로 변경 <<--
        -  색깔을 이용한 슈팅 or 퍼즐류 게임으로 변경
        -  좀더 깔끔하게 디자인 필요

---
+++ Colour/Assets/Scripts/ObstacleControll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ObstacleControll : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.transform.DOMove(new Vector3(0, -5.5f, 0), 2f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }    
+    }
+
+}
